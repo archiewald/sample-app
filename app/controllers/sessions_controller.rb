6d10f9de6477b9b39bd612817af5231aa9bb75d5
archiewald <artur.kozubek1@gmail.com>
2019-06-13 09:20:56 +0200

    add login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
